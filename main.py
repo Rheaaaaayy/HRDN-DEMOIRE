8aecd288de8660e4dc0a39e739439ac48dae3976
sayhi <1045541554@qq.com>
2019-11-14 20:39:22 +0800

    .

--- main.py
+++ main.py
@@ -246,6 +246,6 @@ if __name__ == '__main__':
     input_ = input_.cuda()
     output = model(input_)
     print(output.size())
-    output.sum.backward()
+    output.sum().backward()
 
     # train()
