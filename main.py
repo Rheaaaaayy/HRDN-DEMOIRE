ae377bcc7ab7bc66f547f1a702f184262bdc5906
sayhi <1045541554@qq.com>
2019-11-14 21:08:30 +0800

    .

--- main.py
+++ main.py
@@ -132,7 +132,10 @@ def train(**kwargs):
 
             outputs = model(moires)
             loss = criterion(outputs, clears)
-            psnr = colour.utilities.metric_psnr(outputs.cpu().numpy(), clears.cpu().numpy())
+
+            moires = moires.detach().cpu().numpy()
+            clears = clears.cpu().numpy()
+            psnr = colour.utilities.metric_psnr(outputs, clears)
             #saocaozuo gradient accumulation
             loss = loss/accumulation_steps
             loss.backward()
@@ -145,8 +148,8 @@ def train(**kwargs):
             psnr_meter.add(psnr)
 
             if opt.vis and (ii + 1) % opt.plot_every == 0: #20个batch画图一次
-                vis.images(moires.detach().cpu().numpy()[:40], win='moire_image')
-                vis.images(clears.cpu().numpy()[:40], win='clear_image')
+                vis.images(moires, win='moire_image')
+                vis.images(clears, win='clear_image')
 
                 vis.plot('train_loss', loss_meter.value()[0]) #meter.value() return 2 value of mean and std
                 vis.log("epoch:{epoch}, lr:{lr}, train_loss:{loss}, train_psnr:{train_psnr}".format(epoch=epoch,
