586b9c2ce1335da11027734fa572fefbefb12266
sayhi <1045541554@qq.com>
2019-11-16 19:37:03 +0800

    .

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def train(**kwargs):
 
     model = model.to(opt.device)
 
-    val_loss, val_psnr = val(model, val_dataloader)
+    val_loss, val_psnr = val(model, val_dataloader, vis)
     print(val_loss, val_psnr)
 
     criterion = nn.MSELoss(reduction='mean')
