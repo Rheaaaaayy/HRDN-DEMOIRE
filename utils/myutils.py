d9686fa9a0dc2759a731c1ba8b0f8b848a9cfbd4
sayhi <1045541554@qq.com>
2019-11-24 20:55:26 +0800

    modify the pixel_unshffule to cuda

--- utils/myutils.py
+++ utils/myutils.py
@@ -92,26 +92,29 @@ def tensor2im(input_image, imtype=np.uint8):
     return image_numpy
 
 
-def pixel_unshuffle(batch_input, shuffle_scale = 2, device=torch.device('cpu')):
-    batch_input = batch_input.to(device)
+def pixel_unshuffle(batch_input, shuffle_scale = 2, device=torch.device('cuda')):
     batch_size = batch_input.shape[0]
     num_channels = batch_input.shape[1]
     height = batch_input.shape[2]
     width = batch_input.shape[3]
 
     conv1 = nn.Conv2d(1, 1, 2, 2, bias=False)
+    conv1 = conv1.to(device)
     conv1.weight.data = torch.from_numpy(np.array([[1, 0],
                                                     [0, 0]], dtype='float32').reshape((1, 1, 2, 2)))
     conv2 = nn.Conv2d(1, 1, 2, 2, bias=False)
+    conv2 = conv2.to(device)
     conv2.weight.data = torch.from_numpy(np.array([[0, 1],
                                                     [0, 0]], dtype='float32').reshape((1, 1, 2, 2)))
     conv3 = nn.Conv2d(1, 1, 2, 2, bias=False)
+    conv3 = conv3.to(device)
     conv3.weight.data = torch.from_numpy(np.array([[0, 0],
                                                     [1, 0]], dtype='float32').reshape((1, 1, 2, 2)))
     conv4 = nn.Conv2d(1, 1, 2, 2, bias=False)
+    conv4 = conv4.to(device)
     conv4.weight.data = torch.from_numpy(np.array([[0, 0],
                                                     [0, 1]], dtype='float32').reshape((1, 1, 2, 2)))
-    Unshuffle = torch.ones((batch_size, 4, height//2, width//2), requires_grad=False)
+    Unshuffle = torch.ones((batch_size, 4, height//2, width//2), requires_grad=False).to(device)
 
     for i in range(num_channels):
         each_channel = batch_input[:, i:i+1, :, :]
@@ -123,4 +126,4 @@ def pixel_unshuffle(batch_input, shuffle_scale = 2, device=torch.device('cpu')):
         Unshuffle = torch.cat((Unshuffle, result), dim=1)
 
     Unshuffle = Unshuffle[:, 4:, :, :]
-    return Unshuffle.detach().cuda()
+    return Unshuffle.detach()
