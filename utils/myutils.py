919aefd5e826f0d0865cd1bd92b81b66e8796de5
sayhi <1045541554@qq.com>
2019-11-24 22:52:26 +0800

    .

--- utils/myutils.py
+++ utils/myutils.py
@@ -80,7 +80,7 @@ class Weighted_Loss(nn.Module):
         c_loss = self.Charbonnier_loss(X, Y)
         s_loss = self.Sobel_Loss(X, Y)
         loss = c_loss * 0.5 + s_loss * 0.5
-        return c_loss
+        return loss
 
 
 # Converts a Tensor into an image array (numpy)
