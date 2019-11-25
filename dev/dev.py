16b097bf8db1bffc3d10849798fbc2212784b2ab
sayhi <1045541554@qq.com>
2019-11-25 16:12:57 +0800

    .

--- dev/dev.py
+++ dev/dev.py
@@ -148,6 +148,10 @@ def train(**kwargs):
     for epoch in range(opt.max_epoch):
         if epoch < last_epoch:
             continue
+        if epoch == 50:
+            opt.loss_alpha = 0.6
+        if epoch == 100:
+            opt.loss_alpha = 0.5
         loss_meter.reset()
         psnr_meter.reset()
         loss_list = []
