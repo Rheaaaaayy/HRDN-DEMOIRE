0cd48643a8ac114c458211f99bfdf8f2a6cc2ea3
sayhi <1045541554@qq.com>
2019-11-27 11:34:36 +0800

    .

--- dev/dev.py
+++ dev/dev.py
@@ -62,7 +62,7 @@ class Config(object):
     train_batch_size = 32 #train的维度为(10, 3, 256, 256) 一个batch10张照片，要1000次iter
     val_batch_size = 32
     max_epoch = 400
-    lr = 1e-6
+    lr = 1e-5
     lr_decay = 0.90
     beta1 = 0.5  # Adam优化器的beta1参数
     accumulation_steps = 1 #梯度累加的参数
@@ -70,7 +70,7 @@ class Config(object):
 
     vis = False if temp_winorserver else True
     env = 'demoire'
-    plot_every = 20 #每隔20个batch, visdom画图一次
+    plot_every = 100 #每隔20个batch, visdom画图一次
 
     save_every = 5  # 每5个epoch保存一次模型
     model_path = None #'checkpoints/HRnet_211.pth'
@@ -198,7 +198,7 @@ def train(**kwargs):
             psnr = colour.utilities.metric_psnr(outputs, clears)
             psnr_meter.add(psnr)
 
-            if opt.vis and (ii + 1) % opt.plot_every == 0: #20个batch画图一次
+            if opt.vis and (ii + 1) % opt.plot_every == 0: #100个batch画图一次
                 vis.images(moires, win='moire_image')
                 vis.images(outputs, win='output_image')
                 vis.text("current outputs_size:{outputs_size},<br/> outputs:{outputs}<br/>".format(
