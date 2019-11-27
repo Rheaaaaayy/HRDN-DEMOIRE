54ae9d6cbada2d9e7ae83f0177df191b0bba0cda
sayhi <1045541554@qq.com>
2019-11-27 11:14:05 +0800

    .

--- dev/dev.py
+++ dev/dev.py
@@ -265,10 +265,8 @@ def val(model, dataloader, vis=None):
     for ii, (val_moires, val_clears) in tqdm(enumerate(dataloader)):
         val_moires = val_moires.to(opt.device)
         val_clears = val_clears.to(opt.device)
-        val_output_list, val_edge_outputs = model(val_moires)
-        val_outputs = val_output_list[0]
-        print(val_outputs.size())
-        print(val_clears.size())
+        val_output_list, val_edge_output_list = model(val_moires)
+        val_outputs, val_edge_outputs = val_output_list[0], val_edge_output_list[0]
 
         c_loss = criterion_c(val_outputs, val_clears)
         s_loss = criterion_s(val_edge_outputs, val_clears)
