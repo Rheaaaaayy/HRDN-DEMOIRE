6e1624118d20ec3e7acc13a3dc16ca26218c98ae
sayhi <1045541554@qq.com>
2019-12-15 14:00:15 +0800

    .

--- run_ablation.sh
+++ run_ablation.sh
@@ -1,2 +1,3 @@
 nohup python -u ablation/ablation_sobel.py >ablation_sobel.out 2>&1 &
-
+# nohup python -u ablation/ablation_finallayer.py >ablation_finallayer.out 2>&1 &
+# nohup python -u ablation/ablation_fuse.py >ablation_fuse.out 2>&1 &
